fc64c8653b39338cd38dae4cde33ff334eb83b24
Juan Camilo Céspedes Romero <juankamilocromero@gmail.com>
2023-04-18 15:28:40 -0500

    primer commit de mi marca personal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app/header/header.component';
+import { CardsBodyComponent } from './app/cards-body/cards-body.component';
+import { NavToolComponent } from './app/nav-tool/nav-tool.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardsBodyComponent,
+    NavToolComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +21,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+}
---
+++ src/app/app/nav-tool/nav-tool.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-tool',
+  templateUrl: './nav-tool.component.html',
+  styleUrls: ['./nav-tool.component.css']
+})
+export class NavToolComponent {
+
+}
