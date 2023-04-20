32e56d0db1e3df06d3dd132230f66f9acf1ccd9d
Juan Camilo Céspedes Romero <juankamilocromero@gmail.com>
2023-04-19 22:05:48 -0500

    Commit con videos finales

--- src/app/app/header/header.component.ts
+++ src/app/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  
+  shouldHide = false;
+
+  constructor(private renderer: Renderer2) { }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+
+    if (window.pageYOffset > 3000) {
+      this.shouldHide = true;
+      this.renderer.setStyle(document.body, 'padding-top', '50px');
+    } else {
+      this.shouldHide = false;
+      this.renderer.setStyle(document.body, 'padding-top', '0');
+    }
+  }
 
 }
